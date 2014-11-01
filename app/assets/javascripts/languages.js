6eaa4947b8749b0b1fe30f9856116e5692fca054
Jason Lee <chsienlee@gmail.com>
2014-11-01 19:05:47 +0800

    itemCtrl

--- app/assets/javascripts/languages.js
+++ app/assets/javascripts/languages.js
@@ -23,3 +23,27 @@ languageApp.controller('formCtrl', function($scope, $element, $http) {
 languageApp.controller('listCtrl', function($scope, $http) {
   $scope.update_languages();
 });
+
+languageApp.controller('itemCtrl', function($scope, $http) {
+  $scope.isEdit = false;
+
+  // 展開或關閉編輯模式
+  $scope.edit_toggle = function(isEdit) {
+    $scope.isEdit = isEdit;
+    $scope.onedit_language = angular.copy($scope.language);
+  };
+
+  $scope.update = function() {
+    $http.put('/languages/' + $scope.onedit_language.id + '.json', $scope.onedit_language).
+    success(function(response) {
+      $scope.update_languages();
+    });
+  };
+
+  $scope.delete = function() {
+    $http.delete('/languages/' + $scope.language.id + '.json').
+    success(function(response) {
+      $scope.update_languages();
+    });
+  };
+});
